2985a779b431352e1e33d1df64ab4d4f47c64275
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2016-09-21 01:43:58 -0400

    finished html, set up firebase

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB8N-dYGdBI3HW3toJEs3f6v2snNicTT9U",
+    authDomain: "train-master.firebaseapp.com",
+    databaseURL: "https://train-master.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "688822092647"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
